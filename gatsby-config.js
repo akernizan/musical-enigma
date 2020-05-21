4c5b3c05b11da792fccc87bb13ebfa91ed172077
Akosua Kernizan <akosua@Akosuas-Air.home>
2020-05-21 09:05:03 -0400

    updated GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,6 @@
-const dotenv = require('dotenv');
-
-dotenv.config();
-const e = process.env;
+require('dotenv').config({
+  path: `.env.production`
+})
 
 module.exports = {
   siteMetadata: {
@@ -14,7 +13,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId: e.GOOGLE_ANALYTICS_ID,
+        trackingId: process.env.GOOGLE_ANALYTICS_ID,
         // Defines where to place the tracking script - `true` in the head and `false` in the body
         head: true,
         // Delays sending pageview hits on route update (in milliseconds)
