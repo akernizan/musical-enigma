7ded15871451ba937d93f76b8b2c40c7306a1565
Akosua Kernizan <akosua@akosuas-air.home>
2020-06-04 10:51:50 -0400

    added link

--- src/pages/links.js
+++ src/pages/links.js
@@ -11,6 +11,9 @@ const Links = () => (
       <h1>Akosua Kernizan</h1>
       <h2>Web Developer</h2>
       <div className="link__button">
+        <a href="https://docs.google.com/document/d/1CjZMORRVuv-I-qo4B0YfmOTqIOa3GUS207t5iuLZmyA/mobilebasic" target="_blank" rel="noopener noreferrer">BLM Charites</a>
+      </div>
+      <div className="link__button">
         <a href="https://www.linkedin.com/in/akosuakernizan/" target="_blank" rel="noopener noreferrer">Linkedin</a>
       </div>
       <div className="link__button">
